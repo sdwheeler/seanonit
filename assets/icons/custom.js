40202e884733c4f0bd6c16e1773f4bbd8963723b
Sean Wheeler <sean.wheeler@microsoft.com>
2024-10-28 11:11:23 -0500

    Update socials

--- assets/icons/custom.js
+++ assets/icons/custom.js
@@ -1,5 +1,6 @@
 import {
-    faBlog, faBook, faFile, faNewspaper, faTerminal, faLessThan, faPlay, faScroll, faUserPen
+    faBlog, faBook, faFile, faNewspaper, faTerminal, faLessThan, faPlay, faScroll, faUserPen,
+    faBullhorn
 } from '@fortawesome/free-solid-svg-icons';
 
 import {
@@ -7,14 +8,15 @@ import {
 } from '@fortawesome/free-regular-svg-icons';
 
 import {
-    faAmazon, faGoogle, faMarkdown, faOsi
+    faAmazon, faGoogle, faMarkdown, faOsi, faBluesky, faXTwitter
 } from '@fortawesome/free-brands-svg-icons';
 
 const icons = [
     faBook, faBlog, faFile, faNewspaper, faTerminal, faLessThan, faPlay, faScroll, faUserPen,
+    faBullhorn,
 
     faAddressBook, faFilePdf, faCircleQuestion,
 
-    faAmazon, faGoogle, faMarkdown, faOsi
+    faAmazon, faGoogle, faMarkdown, faOsi, faBluesky, faXTwitter
 ];
 export default icons;
